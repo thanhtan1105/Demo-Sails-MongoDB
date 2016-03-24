e19c5e066e3d436a24c1c61eb3036745f61566b2
Le Thanh Tan <thanhtan1105@gmail.com>
2016-03-24 09:48:16 +0700

    update

--- api/controllers/NotificationController.js
+++ api/controllers/NotificationController.js
@@ -8,22 +8,22 @@ var Noti = require('../models/Notifications');
 
 module.exports = {
 
-  register: function(request, response, next) {
+  register: function(request, response) {
     if (!request.body.appName || !request.body.os || ! request.body.token) {
       return response.json({status: "400", message: "Missing param"});
     }
 
-    Notifications.findOne(request.body.appName, function (error, notification) {
+    Notifications.findOne({appName: request.body.appName}).exec(function (error, notification) {
       if (error) return next(error);
       console.log("Notification :" + notification);
 
       var paramsToken = {
-        token : notification,
+        token : request.body.token,
         kind : request.body.os
       };
 
       if (!notification) {
-        // new app
+        // not found, create new app
         var params = {
           appName: request.body.appName
         };
@@ -42,9 +42,13 @@ module.exports = {
           });
         });
       } else {
-        paramsToken.owner = notification._id;
+        paramsToken.owner = notification;
         Token.createToken(paramsToken, function(error, token) {
-          if (error) return response.json({status: "401", message: "fail create token"});
+          console.log("Token :" + token);
+          if (error) {
+            console.log("Error:" + error);
+            return response.json({status: "401", message: "fail create token"});
+          }
           return response.json({status: "200", message: "Create Success"});
         });
       }
--- api/models/Token.js
+++ api/models/Token.js
@@ -9,33 +9,35 @@ module.exports = {
   schema: true,
   attributes: {
     tokenId: {
-      type: 'string',
-      require: true
+      type: 'string'
+
     },
 
     kind: {
-      type: 'string', // android or iOS
-      require: true
+      type: 'string' // android or iOS
     },
 
     // Add a reference to Notification
     owner: {
       model: 'Notifications'
-    },
-
-    createToken: function createToken(params, callback) {
-      Token.create({
-        tokenId: params["token"],
-        kind: params["kind"],
-        owner: params["owner"]
-
-      }).exec(callback);
-    },
-
-    toJSON: function() {
-      var object = this.toObject();
-      return object;
     }
+  },
+
+  createToken: function createToken(params, callback) {
+    Token.create({
+      tokenId: params["token"],
+      kind: params["kind"],
+      owner: params["owner"]
+
+    }).exec(callback);
+  },
+
+  toJSON: function() {
+    var object = this.toObject();
+    return object;
   }
+
+
+
 };
 
